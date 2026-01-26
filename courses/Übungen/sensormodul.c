b84c28c1063bdcbabecaaa84c5b10dc0c48a828b
Nikolai-Slsr <163042351+Nikolai-Slsr@users.noreply.github.com>
2026-01-26 21:25:01 +0100

    Solved 1. 2. 3.

--- courses/Übungen/sensormodul.c
+++ courses/Übungen/sensormodul.c
@@ -2,13 +2,30 @@
 #include <stdbool.h>
 
 bool uebertemperatur(char* status) {
+    unsigned char temp = *status & 0b01000000;
+    if (temp){return(true);}
+    return(false);
+}
+bool unterspannung(char* status) {
+    unsigned char temp = *status & 0b00100000;
+    if (temp){return(true);}
+    return(false);
+}
+bool fehler(char* status) {
     unsigned char temp = *status & 0b10000000;
     if (temp){return(true);}
     return(false);
 }
+unsigned char messwert(char* status){
+    return(*status & 0b00001111);
+}
 
 int main(){
-    unsigned char status = 0b10100111;
-    printf("Übertemperatur: \n %u ", uebertemperatur(&status));
+    unsigned char status = 0xA7;
+    printf("Übertemperatur: \n %u \n", uebertemperatur(&status));
+    printf("Überspannung: \n %u \n", unterspannung(&status));
+    printf("Fehler: \n %u \n", fehler(&status));
+
+    printf("Messert: \n\n %u \n", messwert(&status));
     return(0);
 }
