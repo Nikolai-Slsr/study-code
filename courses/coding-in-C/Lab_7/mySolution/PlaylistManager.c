80a3b726fe60ee37afcd3e0474c72f21e0b0af7e
Nikolai-Slsr <163042351+Nikolai-Slsr@users.noreply.github.com>
2026-02-20 19:38:14 +0100

    Fix song structure in PlaylistManager and enhance addSong and printPlaylist functions

--- courses/coding-in-C/Lab_7/mySolution/PlaylistManager.c
+++ courses/coding-in-C/Lab_7/mySolution/PlaylistManager.c
@@ -4,8 +4,8 @@
 
 struct song
 {
-    char name;
-    char artist;
+    char name[100];
+    char artist[100];
     struct song * nextSong;
 };
 struct playlist
@@ -16,44 +16,91 @@ struct playlist
 
 
 struct playlist * initPlaylist(){
-    struct playlist *pNewPlaylist = malloc(sizeof pNewPlaylist);
+    struct playlist *pNewPlaylist = malloc(sizeof *pNewPlaylist);
     pNewPlaylist->length = 0;
+    pNewPlaylist->firstSong = NULL;
     return pNewPlaylist;
 }
-struct song * addSong(struct playlist * playlist, char artist[], char name[]){
-    struct song *pCurentSong = playlist->firstSong; 
-    struct song *newSong = malloc(sizeof(newSong));
-    newSong->name = &name;
-    newSong->artist = &artist;
-    for (short index = 0; index <= playlist->length; index++)
+struct song * addSong(struct playlist * playlist, char artist[], char name[]){ //returns NULL if failed, pointer to new song if success
+
+    if (playlist == NULL)
     {
-        if (index = playlist->length)
-        {
-            pCurentSong->nextSong = newSong;
-        }
-        pCurentSong = pCurentSong->nextSong;
+        printf("Playlist is NULL\n");
+        return NULL;
     }
-    playlist->length++;
-    return newSong;
+    
+    struct song *pCurentSong = playlist->firstSong; 
+    struct song *newSong = malloc(sizeof *newSong);
+
+    if (newSong == NULL)
+    {
+        printf("Failed to allocate memory for new song\n");
+        return NULL;
+    }
+    strcpy(newSong->name, name); 
+    strcpy(newSong->artist, artist);
+    newSong->nextSong = NULL;
+
+    if (pCurentSong == NULL) //first song in the playlist
+    {
+        playlist->firstSong = newSong;
+        playlist->length++;
+        return newSong;
+    }
+
+    for (short index = 0; index <= playlist->length; index++) //loop through the playlist to find the last song
+    {
+        if (pCurentSong->nextSong == NULL)
+        {
+            //printf("Adding song: Artist: %s, Name: %s\n", newSong->artist, newSong->name);
+            pCurentSong->nextSong = newSong;
+            playlist->length++;
+            return newSong;
+        }
+        pCurentSong = pCurentSong->nextSong; //move to the next song
+    }
+    return NULL; //should never reach this point
 }
 
 int printPlaylist(struct playlist *pPlaylist){
     if (pPlaylist->length == 0)
     {
-        printf("The playlist is empty");
+        printf("The playlist is empty\n");
         return 1;
     }
+    printf("Playlist: \n");
     struct song *pCurentSong = pPlaylist->firstSong;
     for (short index = 0; index < pPlaylist->length; index++)
     {
-        printf("Artist: %s, Name: %s", pCurentSong->artist, pCurentSong->name);
+        printf("%d. - %s  by  %s \n",index, pCurentSong->name, pCurentSong->artist);
         pCurentSong = pCurentSong->nextSong;
     }
+    return 0;
 }
 
-struct song * deleteFirstSong(){
-
-}
+int deleteFirstSong(struct playlist *pPlaylist){ // returns 0 if success, 1 if failed
+    //get the second song in the playlist and set it as the first song, then free the memory of the first song
+    if (pPlaylist == NULL || pPlaylist->firstSong == NULL)
+    {
+        printf("Playlist is empty or NULL\n");
+        return 1;
+    }
+    if (pPlaylist->firstSong->nextSong == NULL)
+    {
+        struct song *deletedSong = pPlaylist->firstSong;
+        pPlaylist->firstSong = NULL;
+        pPlaylist->length--;
+        free(deletedSong);
+        return 0;
+    }
+    
+    struct song *deletedSong = pPlaylist->firstSong;
+    struct song *newFirstSong = pPlaylist->firstSong->nextSong;
+    pPlaylist->firstSong = newFirstSong;
+    pPlaylist->length--;
+    free(deletedSong);
+    return 0;
+    }
 void deletePlaylist(){
 
 }
@@ -61,8 +108,21 @@ void deletePlaylist(){
 int main(){
     //test current funtions
     struct playlist *myPlaylist = initPlaylist();
-    addSong(myPlaylist, "Artist1", "Name1");
-    addSong(myPlaylist, "Artist2", "Name2");
+    addSong(myPlaylist, "Darci", "On My Own");
+    // addSong(myPlaylist, "Djo", "End of Beginning");
+    // addSong(myPlaylist, "Arctic Monkeys", "I Wanna Be Yours");
+    // addSong(myPlaylist, "Choise Atlantic", "Friends");
+    // addSong(myPlaylist, "Nirvana", "Come As You Are");
+    // addSong(myPlaylist, "Franz Ferdinand", "Take Me Out");
+    // addSong(myPlaylist, "The Rare Occasions", "Notion");
+    // addSong(myPlaylist, "Tame Impala", "The Less I Know The Better");
+    // addSong(myPlaylist, "The Neighbourhood", "Softcore");
     printPlaylist(myPlaylist);
+    deleteFirstSong(myPlaylist);
+    deleteFirstSong(myPlaylist);
+    deleteFirstSong(myPlaylist);
+    deleteFirstSong(myPlaylist);
+    printPlaylist(myPlaylist);
+    return 0;
 
 }
