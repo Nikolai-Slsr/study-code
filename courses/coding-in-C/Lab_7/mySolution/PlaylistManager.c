f13d18d1426a6444f4a48f756912f52f9a72b63c
Nikolai-Slsr <163042351+Nikolai-Slsr@users.noreply.github.com>
2026-02-20 13:13:29 +0100

    Initialize PlaylistManager with song structure and function stubs

---
+++ courses/coding-in-C/Lab_7/mySolution/PlaylistManager.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct song
+{
+    char name[100];
+    char artist[100];
+    struct song * nextSong;
+};
+
+struct song * initPlaylist(){
+
+}
+struct song * addSong(){
+
+}
+
+int printPlaylist(){
+
+}
+
+struct song * deleteFirstSong(){
+
+}
+void deletePlaylist(){
+
+}
+
+int main(){
+
+
+}
