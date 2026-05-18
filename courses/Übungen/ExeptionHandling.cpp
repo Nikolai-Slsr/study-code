513bb07514f1459c85aa8db0b2f1095a6cade776
Nikolai-Slsr <163042351+Nikolai-Slsr@users.noreply.github.com>
2026-05-18 21:21:17 +0200

    Started übung

---
+++ courses/Übungen/ExeptionHandling.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <stdexcept>
+
+class ConfigLoader
+{
+    public:
+        void load(const std::string& filename){
+            if (filename == "missing.cfg"){}
+        }
+        
+
+};
